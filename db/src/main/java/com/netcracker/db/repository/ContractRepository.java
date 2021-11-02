23b9753f87fc915b9044738f13719f5ce28ea89d
Eygonb <eygonb@gmail.com>
2021-11-02 22:58:54 +0300

    Add method declarations of the List interface and implement methods in class MyArrayList

--- db/src/main/java/com/netcracker/db/repository/ContractRepository.java
+++ db/src/main/java/com/netcracker/db/repository/ContractRepository.java
@@ -1,4 +1,32 @@
 package com.netcracker.db.repository;
 
+import com.netcracker.db.entity.Contract;
+import com.netcracker.utils.List;
+
 public interface ContractRepository {
+    /**
+     * Saves the contract if it is not in the repository, updates if it exists.
+     * @param contract contract to be appended or updated to this repository
+     */
+    void save(Contract contract);
+
+    /**
+     * Saves contracts from the list that are not in the repository, updates which are.
+     * @param list list of contracts to be appended or updated to this repository
+     */
+    void saveAll(List<Contract> list);
+
+    /**
+     * Returns the contract with the specified identifier, or null if there is no such contract.
+     * @param id id of the contract to return
+     * @return the contract with the specified identifier, or null if there is no such contract
+     */
+    Contract getById(Integer id);
+
+    /**
+     * Remove the contract with the specified identifier and return true, or return false if there is no such contract.
+     * @param id id of the contract to remove
+     * @return true if the contract has been deleted
+     */
+    boolean removeById(Integer id);
 }
--- db/src/main/java/com/netcracker/db/repository/impl/ContractRepositoryImpl.java
+++ db/src/main/java/com/netcracker/db/repository/impl/ContractRepositoryImpl.java
@@ -1,7 +1,78 @@
 package com.netcracker.db.repository.impl;
 
+import com.netcracker.db.entity.Contract;
 import com.netcracker.db.repository.ContractRepository;
+import com.netcracker.utils.List;
+import com.netcracker.utils.impl.MyArrayList;
+import java.util.function.Predicate;
 
 public class ContractRepositoryImpl implements ContractRepository {
+    private List<Contract> contracts;
 
+    public ContractRepositoryImpl() {
+        contracts = new MyArrayList<>();
+    }
+
+    /**
+     * Saves the contract if it is not in the repository, updates if it exists.
+     *
+     * @param contract contract to be appended or updated to this repository
+     */
+    @Override
+    public void save(Contract contract) {
+        if(contract == null) return;
+
+        for(int i = 0; i < contracts.size(); i++) {
+            if(contract.getId().equals(contracts.get(i).getId())) {
+                contracts.set(i, contract);
+                return;
+            }
+        }
+        contracts.add(contract);
+    }
+
+    /**
+     * Saves contracts from the list that are not in the repository, updates which are.
+     *
+     * @param list list of contracts to be appended or updated to this repository
+     */
+    @Override
+    public void saveAll(List<Contract> list) {
+        for(int i = 0; i < list.size(); i++) {
+            save(list.get(i));
+        }
+    }
+
+    /**
+     * Returns the contract with the specified identifier, or null if there is no such contract.
+     *
+     * @param id id of the contract to return
+     * @return the contract with the specified identifier, or null if there is no such contract
+     */
+    @Override
+    public Contract getById(Integer id) {
+        for(int i = 0; i < contracts.size(); i++) {
+            if(id.equals(contracts.get(i).getId())) {
+                return contracts.get(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Remove the contract with the specified identifier and return true, or return false if there is no such contract.
+     *
+     * @param id id of the contract to remove
+     * @return true if the contract has been deleted
+     */
+    @Override
+    public boolean removeById(Integer id) {
+        for(int i = 0; i < contracts.size(); i++) {
+            if(id.equals(contracts.get(i).getId())) {
+                contracts.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
